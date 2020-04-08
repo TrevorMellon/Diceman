a58c7d35c711fb13941cdca19f61f6b41cec1b3b
Codified <codified@protonmail.com>
2020-04-08 13:14:43 +0100

    Add dice notation

--- bot.js
+++ bot.js
@@ -62,5 +62,97 @@ client.on('message', msg => {
             msg.reply("Using a 6-sided die, you rolled: " + r);
         }
     }
+    else if(v[0] === '!dice') {
+        if(v.length == 2) {
+            var re = new RegExp('([0-9]+)d([0-9]+)', 'i');
+            var re2 = new RegExp('([0-9]+)d([0-9]+)([\+\-])([0-9]+)', 'i');
+            var m = msg.content.toString();
+            
+            var b = re.test(m);
+            var b2 = re2.test(m);
+            let match;
+
+            if(b2){
+                match = re2.exec(m);
+
+                console.log("match 0:"+match[1]);
+                console.log("match 1:"+match[2]);
+                console.log("match 3:"+match[3]);
+                console.log("match 4:"+match[4]);
+
+                var rolls = match[1];
+                var die = match[2];
+
+                var modifier = match[3];
+                modifier = modifier.toString();
+                var mmod = match[4];
+
+                var mmodi = parseInt(mmod);
+
+                var diei = parseInt(die);
+                var rollsi = parseInt(rolls);
+
+                var strmsg = "Using a " + diei + "-sided die. \n"
+                strmsg +=    "============================\n"
+
+                var count = 0;
+
+                for(var i = 0; i<rollsi; i++) {
+                    var r = Math.floor((Math.random() * diei) + 1);
+                    //msg.reply("Using a "+xi+"-sided die, you rolled: " + r);
+                    strmsg += "Roll #" + (i+1) + ": " + r +"\n";
+                    count += r;
+                }
+                strmsg += "============================\n"
+                strmsg += "Roll Amount: " + count + "\n";
+                strmsg += "Modifier: " +modifier +mmodi+"\n"
+                strmsg += "============================\n"
+
+                var count2 = count;
+
+                if(modifier === "+") {
+                    count2 = count + mmodi;
+                }
+                else if(modifier === "-") {
+                    count2 = count - mmodi;
+                }
+
+                strmsg += "Modified Total Amount: " + count2 + "\n";
+
+                msg.reply(strmsg);
+
+            }
+            else if(b) {
+                match = re.exec(m);
+
+                var rolls = match[1];
+                var die = match[2];
+
+                console.log("match 0:"+match[1]);
+                console.log("match 1:"+match[2]);
+
+                var diei = parseInt(die);
+                var rollsi = parseInt(rolls);
+
+                var strmsg = "Using a " + diei + "-sided die. \n"
+                strmsg +=    "============================\n"
+
+                var count = 0;
+
+                for(var i = 0; i<rollsi; i++) {
+                    var r = Math.floor((Math.random() * diei) + 1);
+                    //msg.reply("Using a "+xi+"-sided die, you rolled: " + r);
+                    strmsg += "Roll #" + (i+1) + ": " + r +"\n";
+                    count += r;
+                }
+                strmsg += "============================\n"
+                strmsg += "\nTotal Amount: " + count + "\n";
+
+                msg.reply(strmsg);
+            }
+            
+
+        }
+    }
     
 });
